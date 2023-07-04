a8333b5995490ee1c1861dc07c98097d96529b6f
nikola-tarasov <nikola-tarasov@mail.ru>
2023-07-04 13:54:18 +0300

    callback_fun

--- lesson_7/database.py
+++ lesson_7/database.py
@@ -10,6 +10,7 @@ cur = db.cursor()
 async def db_start():
         cur.execute("CREATE TABLE IF NOT EXISTS accounts("
                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "tg_id INTEGER,"
                    "cart_id TEXT)")
         cur.execute("CREATE TABLE IF NOT EXISTS items("
                     "i_id INTEGER PRIMARY KEY AUTOINCREMENT,"
--- lesson_7/main.py
+++ lesson_7/main.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup,ReplyKeyboardMarkup, KeyboardButton
-from aiogram.dispatcher.filters import Filter
-import re
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup,InlineKeyboardButton
+
 from lesson_7 import database as db
 
 
@@ -18,10 +17,36 @@ TOKEN_API = "6181318829:AAEunNJz8JCNwOAb0UBRabRAmvkHjm74LEo"
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+b1 = KeyboardButton(text='Каталог')
+b2 = KeyboardButton(text='Корзина')
+b3 = KeyboardButton(text='прочее')
+kb.add(b1).add(b2).add(b3)
+
+ikb = InlineKeyboardMarkup(row_width=2)
+ib1 = InlineKeyboardButton(text='Кроссовки', url='https://mail.ru/')
+ib2 = InlineKeyboardButton(text='Футболка', callback_data='t-shirts')
+ikb.add(ib1, ib2)
+
+
 @dp.message_handler(commands=['start'])
-async def start_commands(message: types.Message):
-        await message.answer(text='Привет я телеграмм Бот!')
+async def start_cmd(message: types.Message):
+        await message.answer(text='Бот!',reply_markup=kb)
         await message.delete()
+
+
+@dp.message_handler(text = 'Каталог')
+async def katalog_cmd(message: types.Message):
+     await message.answer(text='Товаров нет',
+                   reply_markup=ikb)
+
+@dp.callback_query_handler()
+async def callback_query_keyboard(callback_query: types.CallbackQuery):
+    if callback_query.data == 't-shirts':
+        await bot.send_message(chat_id = callback_query.from_user.id,text='Вы выбрали 0')
+    else:
+        await message.reaply('Error')
+
 
 
 if __name__=='__main__':
